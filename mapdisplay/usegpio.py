9f010c44800d7a2720a1095c19a8bf02105189a9
Ian Sibley <isibley@purdue.edu>
2019-02-08 20:33:49 +0000

    Button: 3.3V - Resistor - Button - pin10; GPIO works and is able to input and output (output pin 12)

--- mapdisplay/usegpio.py
+++ mapdisplay/usegpio.py
@@ -5,14 +5,21 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
+ # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 finish = start = time.time()
 
-while(finish - start < 4):
-    print(finish-start)
+while(True):
     GPIO.output(12, GPIO.HIGH)
-    time.sleep(.1)
-    GPIO.output(12, GPIO.LOW)
-    time.sleep(.1)
-    
-    finish = time.time()
+    print(finish-start)
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed!")
 
+    time.sleep(1)
+    print(finish-start)
+    GPIO.output(12, GPIO.LOW)
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed!")
+
+    time.sleep(1)
